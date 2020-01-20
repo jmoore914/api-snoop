488d2119dfb53fac8b886a3a11aa0fe7dd9f3d56
jmoore914 <jmoore914@gmail.com>
2020-01-20 13:20:56 -0500

    Fixed public path

--- Vue.config.js
+++ Vue.config.js
@@ -1,4 +1,4 @@
-const publicPath = process.env.NODE_ENV==='development' ? '' : `${process.cwd()}/dist/`;
+const publicPath = process.env.NODE_ENV==='development' ? '' : '\./';
 
 module.exports = {
 	publicPath: publicPath
